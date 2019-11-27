3cda96545e4e54c3859d16bb30213888c61e4af7
Hans Jerry Illikainen <hji@dyntopia.com>
2019-11-27 14:03:27 +0000

    ansible: add CI setup

---
+++ projects/ansible/ci.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+ci_install() {
+    if [[ "$PYTHON" == "2.7" ]]; then
+        sudo apt-get install --yes python-apt
+        virtualenv -p "python$PYTHON" --system-site-packages venv
+    else
+        sudo apt-get install --yes python3-apt python3-venv python3-dev
+        "python$PYTHON" -m venv --system-site-packages venv
+    fi
+
+    # shellcheck disable=SC1091
+    source venv/bin/activate
+    pip install \
+        -r requirements.txt \
+        -r docs/docsite/requirements.txt \
+        -r test/lib/ansible_test/_data/requirements/coverage.txt \
+        -r test/lib/ansible_test/_data/requirements/integration.txt \
+        -r test/lib/ansible_test/_data/requirements/network-integration.txt \
+        -r test/lib/ansible_test/_data/requirements/units.txt \
+        -r test/sanity/requirements.txt \
+        -r test/units/requirements.txt
+}
+
+ci_build() {
+    # shellcheck disable=SC1091
+    source venv/bin/activate
+
+    if test "$DOCS" = 1; then
+        make webdocs
+    fi
+
+    make install
+}
+
+ci_test() {
+    # ansible-test --changed doesn't seem to work reliably, so determine the
+    # target to test based on the branch name instead.  Meh...
+    local target
+    target="$(echo "$APPVEYOR_REPO_BRANCH" \
+        |sed -E -e 's/^.*-?[0-9]+-//' -e 's/-/_/g')"
+
+    if [[ -z "$target" ]]; then
+        echo "WARNING: unknown target" >&2
+        exit 0
+    fi
+
+    # shellcheck disable=SC1091
+    source venv/bin/activate
+
+    ansible-test units --python "$PYTHON" --coverage "$target"
+    ansible-test integration --python "$PYTHON" --coverage \
+                 --allow-root --allow-destructive "$target"
+    ansible-test coverage report --python "$PYTHON" --include "*$target*"
+}
