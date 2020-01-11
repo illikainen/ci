79c1570a360adcaf5e034cb450bda6a4127c06a1
Hans Jerry Illikainen <hji@dyntopia.com>
2020-01-11 09:02:22 +0000

    git: update pipeline for the project-specific docker images

--- projects/git/ci.sh
+++ projects/git/ci.sh
@@ -1,88 +1,35 @@
 #!/usr/bin/env bash
 
-ci_install() {
-    if ci_is_alpine; then
-        ci_sudo apk update
-        ci_sudo apk add \
-                asciidoc \
-                autoconf \
-                build-base \
-                curl-dev \
-                diffutils \
-                findutils \
-                gettext-dev \
-                gnupg \
-                grep \
-                openssh-client \
-                pcre2-dev \
-                pkgconf \
-                python \
-                subversion \
-                tcl \
-                unzip
-    elif ci_is_debian || ci_is_ubuntu; then
-        ci_sudo cp /etc/apt/sources.list /etc/apt/sources.list.d/src.list
-        ci_sudo sed -i 's/^deb[ \s]/deb-src /' /etc/apt/sources.list.d/src.list
+set -o errexit
+set -o nounset
+set -o pipefail
 
-        ci_sudo apt-get update
-        ci_sudo apt-get --assume-yes build-dep git
-        ci_sudo apt-get --assume-yes install autoconf automake gnupg gpgsm
-    elif ci_is_centos || ci_is_fedora; then
-        ci_sudo dnf --assumeyes install \
-                acl \
-                asciidoc \
-                autoconf \
-                automake \
-                diffutils \
-                expat-devel \
-                findutils \
-                gawk \
-                gcc \
-                gcc-c++ \
-                gettext-devel \
-                gnupg2 \
-                gnupg2-smime \
-                libcurl-devel \
-                libsecret-devel \
-                make \
-                openssl-devel \
-                pcre2-devel \
-                perl-Memoize \
-                pkgconfig \
-                python2 \
-                python3 \
-                subversion \
-                subversion-perl \
-                tcl \
-                tk \
-                xmlto \
-                zlib-devel
-        ci_sudo ln -s /usr/bin/python2 /usr/bin/python
-    fi
-}
+export GIT_TEST_OPTS="--immediate --tee --verbose-log --root=t/test-results"
+export CI_DESTDIR="${HOME}/build/git"
+export PATH="${CI_DESTDIR}/bin:${PATH}"
 
 ci_build() {
     if [[ -n "${CI_TRIGGER_COMMIT:-}" ]]; then
         git reset --hard "$CI_TRIGGER_COMMIT"
     fi
-    ci_info "building $(git rev-parse HEAD)"
+    echo "building $(git rev-parse HEAD)"
 
-    make configure
-    {
-        echo "DEVELOPER = 1"
-        echo "DEVOPTS = pedantic"
-        echo "CFLAGS += -fno-omit-frame-pointer -fstack-protector-all"
-        echo "LDFLAGS += -Wl,-z,relro -Wl,-z,now"
-    } > config.mak
+    if [[ ! -f "configure" ]]; then
+        make configure
+        {
+            echo "DEVELOPER = 1"
+            echo "DEVOPTS = pedantic"
+            echo "CFLAGS += -std=c99 -fstack-protector-all"
+            echo "LDFLAGS += -Wl,-z,relro -Wl,-z,now"
+        } > config.mak
+        ./configure --prefix="$CI_DESTDIR"
+    fi
 
-    ./configure --prefix="$CI_DESTDIR"
     make -j"$(nproc)"
     make install
 }
 
 ci_test() {
-    export PATH="${CI_DESTDIR}/bin:${PATH}"
-
     key="Signed-off-by"
     if ! git log -1 --format="%(trailers:key=$key)" |grep "^$key"; then
         echo "missing trailer $key" >&2
@@ -94,10 +41,12 @@ ci_test() {
         exit 1
     fi
 
-    # FIXME
-    if ci_is_alpine; then
-        export GIT_SKIP_TESTS="t4061 t5616 t5703"
-    fi
-
     make test
 }
+
+main() {
+    ci_build
+    ci_test
+}
+
+main "$@"
